214052ace386762eb1ea5e76b3225a49a465bbdf
Datngo0506 <91180042+Datngo0506@users.noreply.github.com>
2024-12-09 18:34:19 +0700

    hiuthu2

--- shop_c/webs/src/main/java/webshop/controller/LoginController.java
+++ shop_c/webs/src/main/java/webshop/controller/LoginController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import webshop.dao.AccountDAO;
 import webshop.dao.RuleDAO;
@@ -21,7 +22,7 @@ public class LoginController {
     @Autowired
     private RuleDAO rule;
 
-    @RequestMapping("/login-auth")
+    @RequestMapping(value = "/login-auth", method = RequestMethod.POST)
     public String login(
             @RequestParam("username") String username,
             @RequestParam("password") String password,
